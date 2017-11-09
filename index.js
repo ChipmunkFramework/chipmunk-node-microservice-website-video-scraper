2aed8c060d645c5297a8b3c6870638ace8f65029
jnbdz <jnbdzjnbdz@gmail.com>
2017-11-09 02:25:14 -0500

    Added other basic files.

---
+++ index.js
@@ -0,0 +1,35 @@
+/**
+ * @file
+ * @version 1.0.0
+ * {@link https://github.com/ChipmunkFramework/chipmunk-node-microservice-website-video-scraper GitHub}
+ */
+
+'use strict';
+
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let { buildSchema } = require('graphql');
+
+// Construct a schema, using GraphQL schema language
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
+`);
+
+// The root provides a resolver function for each API endpoint
+let root = {
+    hello: () => {
+        return 'Hello world!';
+    },
+};
+
+let app = express();
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true,
+}));
+app.listen(4000);
+
+console.log('Running a GraphQL API server at localhost:4000/graphql');
